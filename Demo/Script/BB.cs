e920c0cc81aa2e18daef021bf605ebfa116e0480
Paul <arr.explore@gmail.com>
2025-06-13 02:24:56 -0400

    adding example sfx

---
+++ Demo/Script/BB.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+/// <summary>
+/// This class is a stub, included here for demo purposes only.
+/// It is meant to hold static methods for formatting BBCode text
+/// </summary>
+public static class BB
+{
+    public static string ProcessItemReplacements(string text)
+    {
+        return text;
+    }
+
+}
+
